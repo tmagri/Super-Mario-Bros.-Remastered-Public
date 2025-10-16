44e7da3fea14db8034779fc0af068202b51818fe
JHDev2006 <howelljoe2@gmail.com>
2025-10-16 22:46:02 +0100

    fonts can now have custom variations via jsons

--- Scripts/Parts/FontUpdater.gd
+++ Scripts/Parts/FontUpdater.gd
@@ -1,12 +1,10 @@
 class_name FontUpdater
 extends Node
 
-@onready var resource_getter_smb1 := ResourceGetter.new()
-@onready var resource_getter_smbll := ResourceGetter.new()
-@onready var resource_getter_score := ResourceGetter.new()
+var main_font: Resource = null
+var score_font: Resource = null
 
-@onready var FONT_LL_MAIN = load("uid://djxdgxy1iv8yv")
-@onready var FONT_MAIN = load("uid://bl7sbw4nx3l1t")
+var FONT_MAIN = preload("uid://bl7sbw4nx3l1t")
 @onready var SCORE_FONT = load("uid://cflgloiossd8a")
 
 
@@ -17,6 +15,5 @@ func _ready() -> void:
 	Global.level_theme_changed.connect(update_fonts)
 
 func update_fonts() -> void:
-	FONT_MAIN.base_font = resource_getter_smb1.get_resource(FONT_MAIN.base_font)
-	FONT_LL_MAIN.base_font = resource_getter_smbll.get_resource(FONT_LL_MAIN.base_font)
-	SCORE_FONT.base_font = resource_getter_score.get_resource(SCORE_FONT.base_font)
+	FONT_MAIN.base_font = main_font
+	SCORE_FONT.base_font = score_font

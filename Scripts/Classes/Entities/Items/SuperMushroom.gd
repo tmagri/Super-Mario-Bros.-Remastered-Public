4ff63742f4a043c4c5b7e9a6d5dcd534988be722
Troy Magri <tmagri@outlook.com.au>
2025-12-06 23:36:44 +1100

    Mushroom: Bounce will have gravity of 1. Normal gravity is 3.8

--- Scripts/Classes/Entities/Items/SuperMushroom.gd
+++ Scripts/Classes/Entities/Items/SuperMushroom.gd
@@ -3,10 +3,21 @@ extends PowerUpItem
 const MOVE_SPEED := 60
 
 func _ready() -> void:
-	$BasicEnemyMovement.gravity_scale = 1.0 # Matches NES relative to Global base (10 vs 38)
-	$BasicEnemyMovement.move_speed = 60.0 # Matches NES 1.0 px/frame
+	$BasicEnemyMovement.gravity_scale = 3.8 # Normal volition gravity
+	$BasicEnemyMovement.move_speed = 60.0 
+
+var bounced_by_block := false
+
+func block_bounce_up(block: Node2D) -> void:
+	super.block_bounce_up(block)
+	bounced_by_block = true
+	$BasicEnemyMovement.gravity_scale = 1.0
 
 func _physics_process(delta: float) -> void:
+	if is_on_floor() and velocity.y >= 0:
+		bounced_by_block = false
+		$BasicEnemyMovement.gravity_scale = 3.8
+
 	$BasicEnemyMovement.handle_movement(delta)
 
 func on_area_entered(area: Area2D) -> void:

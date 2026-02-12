a5c08b5abd2fd1c160b785e529051745f6be6310
Troy Magri <tmagri@outlook.com.au>
2026-02-12 18:27:06 +1100

    feat: Implement Mario 35 specific time for Hachisuke

--- Scripts/Classes/Entities/Items/Hachisuke.gd
+++ Scripts/Classes/Entities/Items/Hachisuke.gd
@@ -3,6 +3,9 @@ extends PowerUpItem
 func on_area_entered(area: Area2D) -> void:
 	if area.owner is Player:
 		AudioManager.play_sfx("hachisuke", global_position)
-		$ScoreNoteSpawner.spawn_note(8000)
 		DiscoLevel.combo_amount += 1
+		if Global.current_game_mode == Global.GameMode.MARIO_35:
+			Mario35Handler.add_time(20) # Same time reward as 1-Up
+		else:
+			$ScoreNoteSpawner.spawn_note(8000)
 		queue_free()
